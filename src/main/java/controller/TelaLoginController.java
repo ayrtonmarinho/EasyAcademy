2b7b9dd1a046a4bbf2e522e543c4e9dcd180179b
Arthur250397 <arthur250397@hotmail.com>
2022-06-02 17:02:09 -0300

    Dando continuidade a construção do controller da tela de Login.

--- src/main/java/controller/TelaLoginController.java
+++ src/main/java/controller/TelaLoginController.java
@@ -1,14 +1,34 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TelaLoginController implements Initializable {
+    @FXML
+    private Label msgErroLogin;
+    @FXML
+    private TextField txtUser;
+    @FXML
+    private TextField txtPass;
+    @FXML
+    private ImageView imgLogotipo;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void logar(){
+        boolean isVisible = !msgErroLogin.isVisible();
+        if(isVisible){
+            msgErroLogin.setVisible(true);
+        }
     }
 }
